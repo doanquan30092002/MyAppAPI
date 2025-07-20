de5b0fd6ca9ecf63614045c9405e6d14dc2dce7a
trantiendat2709 <tiendattran086@gmail.com>
2025-07-21 01:24:38 +0700

    add api get round by auction id

---
+++ MyApp.Application/CQRS/GetListAuctionRound/Querries/GetListAuctionRoundRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using MyApp.Application.CQRS.CreateAutionRound.Command;
+
+namespace MyApp.Application.CQRS.GetListAuctionRound.Querries
+{
+    public class GetListAuctionRoundRequest : IRequest<GetListAuctionRoundResponse>
+    {
+        public Guid AuctionId { get; set; }
+    }
+}
---
+++ MyApp.Application/Interfaces/IGetListAuctionRoundRepository/IGetListAuctionRoundRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyApp.Core.Entities;
+
+namespace MyApp.Application.Interfaces.IGetListAuctionRoundRepository
+{
+    public interface IGetListAuctionRoundRepository
+    {
+        Task<List<AuctionRound>> GetAuctionRoundsByAuctionIdAsync(Guid auctionId);
+    }
+}
