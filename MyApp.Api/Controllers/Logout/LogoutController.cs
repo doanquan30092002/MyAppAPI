c878308f70eed126911d5410568a17592a9c3279
DoanDucQuan <doanquan300902@gmail.com>
2025-07-23 18:03:02 +0700

    [Logout]: update Logout

--- MyApp.Api/Controllers/Logout/LogoutController.cs
+++ MyApp.Api/Controllers/Logout/LogoutController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MyApp.Application.Common.Message;
 
 namespace MyApp.Api.Controllers.Logout
@@ -11,7 +10,16 @@ namespace MyApp.Api.Controllers.Logout
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
-            Response.Cookies.Delete("access_token");
+            Response.Cookies.Delete(
+                "access_token",
+                new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.None,
+                    Path = "/", // Rất quan trọng nếu bạn đã set Path khi tạo
+                }
+            );
 
             return Ok(new { Code = 200, Message = Message.LOGOUT_SUCCESS });
         }
