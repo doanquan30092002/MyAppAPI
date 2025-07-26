0d102a9992bc42f324f8b3f054edeb0e8f15a9d5
tungnm <kakal0tdz04@gmail.com>
2025-07-26 14:29:39 +0700

    Get AuctionAssets with higest bid

---
+++ MyApp.Application/CQRS/AuctionAssetsV2/GetAuctionAssetsHighestBid/Queries/GetAssetsHighestBidHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using MyApp.Application.Interfaces.IActionAssetsRepository;
+using MyApp.Core.DTOs.AuctionAssetsDTO;
+
+namespace MyApp.Application.CQRS.AuctionAssetsV2.GetAuctionAssetsHighestBid.Queries
+{
+    public class GetAssetsHighestBidHandler
+        : IRequestHandler<GetAssetsHighestBidRequest, List<AuctionAssetsWithHighestBidResponse>>
+    {
+        private readonly IAuctionAssetsRepository _auctionAssetsRepository;
+
+        public GetAssetsHighestBidHandler(IAuctionAssetsRepository auctionAssetsRepository)
+        {
+            _auctionAssetsRepository = auctionAssetsRepository;
+        }
+
+        public async Task<List<AuctionAssetsWithHighestBidResponse>> Handle(
+            GetAssetsHighestBidRequest request,
+            CancellationToken cancellationToken
+        )
+        {
+            var result =
+                await _auctionAssetsRepository.GetAuctionAssetsWithHighestBidByAuctionIdAsync(
+                    request.AuctionId
+                );
+
+            return result;
+        }
+    }
+}
--- MyApp.Application/Interfaces/IActionAssetsRepository/IAuctionAssetsRepository.cs
+++ MyApp.Application/Interfaces/IActionAssetsRepository/IAuctionAssetsRepository.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyApp.Core.DTOs.AuctionAssetsDTO;
 
 namespace MyApp.Application.Interfaces.IActionAssetsRepository
 {
     public interface IAuctionAssetsRepository
     {
         Task DeleteByAuctionIdAsync(Guid auctionId);
+
+        Task<
+            List<AuctionAssetsWithHighestBidResponse>
+        > GetAuctionAssetsWithHighestBidByAuctionIdAsync(Guid auctionId);
     }
 }
