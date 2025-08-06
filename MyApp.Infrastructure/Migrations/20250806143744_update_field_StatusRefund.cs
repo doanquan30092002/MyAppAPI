199d7b397d474d55a8cc3a70da85da88f1f0a3cc
tungnm <kakal0tdz04@gmail.com>
2025-08-06 21:39:14 +0700

    Update field StatusRefund

---
+++ MyApp.Infrastructure/Migrations/20250806143744_update_field_StatusRefund.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_field_StatusRefund : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRefundRequested",
+                table: "AuctionDocuments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusRefund",
+                table: "AuctionDocuments",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusRefund",
+                table: "AuctionDocuments");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRefundRequested",
+                table: "AuctionDocuments",
+                type: "bit",
+                nullable: true);
+        }
+    }
+}
