f90933323eddfde3e74de35fa97543356de7dc36
tungnm <kakal0tdz04@gmail.com>
2025-08-10 18:35:00 +0700

    Add realtime to auctionroud enter price

---
+++ MyApp.Application/Common/Services/AuctionRoundPriceHub/AuctionRoundPriceHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MyApp.Application.Common.Services.AuctionRoundPriceHub
+{
+    public class AuctionRoundPriceHub : Hub
+    {
+        public async Task JoinGroup(string auctionRoundId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, auctionRoundId);
+        }
+    }
+}
